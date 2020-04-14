945bc23f0126bf5e2abb72d24c0f14f00a550734
cnickels21 <cnickels_21@yahoo.com>
2020-04-14 09:50:57 -0500

    local host listening on 3000

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict'; 
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
